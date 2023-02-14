be7c70346a3b0892c6ee081313a8e57aa0842faa
AmmarMansab <81436005+AmmarMansab@users.noreply.github.com>
2023-02-14 23:31:26 +0500

    master

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import {increment, decrement, setUserAction} from './actions';
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const user = useSelector((state) => state.user);
+
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Redux</h1>
+      <h1>Counter {counter}</h1>
+      <div>
+        <button onClick={()=>dispatch(increment())}>+</button>
+        <button onClick={()=>dispatch(decrement())}>-</button>
+      </div>
+      {!user && (<button onClick={()=>dispatch(setUserAction({id:1, name:"Ammar"}))}>Set User</button>)}
+      {user && <p>{user.name}</p>}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,66 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { createStore } from "redux";
+import myReducers from "./reducers";
+import { Provider } from "react-redux";
+
+    // Changing it by creating index.js in reducers //
+// import counterReducer from "./reducers/Counter";
+// import userReducer from "./reducers/User";
+// const store = createStore(counterReducer, userReducer);
+// now
+//const store = createStore(myReducers); // add extenstion
+const store = createStore(myReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+/*
+import { createStore } from "redux";
+
+// actions
+const increment = () => {
+  return {
+    type: "INCREMENT",
+  };
+};
+const decrement = () => {
+  return {
+    type: "DECREMENT",
+  };
+};
+
+// reducers
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+// store -> Globalized satate
+const store = createStore(counter);
+
+// display
+store.subscribe(()=>console.log(store.getState()));
+
+// dispatch
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(decrement());
+*/
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Add provider
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
